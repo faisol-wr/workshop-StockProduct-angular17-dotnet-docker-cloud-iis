46271a459d8e0a8798766770ba2105340b5021a6
Faisol <faisol.wr@gmail.com>
2024-07-04 21:01:17 +0700

    [Create] layout component

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -4,26 +4,33 @@ import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { FrontLayoutComponent } from './layouts/front-layout/front-layout.component';
 
 export const routes: Routes = [
   {
     path: '',
-    component: HomeComponent,
-  },
-  {
-    path: 'about',
-    component: AboutComponent,
-  },
-  {
-    path: 'contact',
-    component: ContactComponent,
-  },
-  {
-    path: 'login',
-    component: LoginComponent,
-  },
-  {
-    path: 'register',
-    component: RegisterComponent,
+    component: FrontLayoutComponent,
+    children: [
+      {
+        path: '',
+        component: HomeComponent,
+      },
+      {
+        path: 'about',
+        component: AboutComponent,
+      },
+      {
+        path: 'contact',
+        component: ContactComponent,
+      },
+      {
+        path: 'login',
+        component: LoginComponent,
+      },
+      {
+        path: 'register',
+        component: RegisterComponent,
+      },
+    ],
   },
 ];
---
+++ client/src/app/layouts/front-layout/front-layout.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { NavbarComponent } from '../../navbar/navbar.component';
+import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from '../../footer/footer.component';
+
+@Component({
+  selector: 'app-front-layout',
+  standalone: true,
+  imports: [RouterOutlet, NavbarComponent, FooterComponent],
+  templateUrl: './front-layout.component.html',
+  styleUrl: './front-layout.component.scss',
+})
+export class FrontLayoutComponent {}
