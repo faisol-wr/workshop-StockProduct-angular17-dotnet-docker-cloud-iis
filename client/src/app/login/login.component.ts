0a0e2a530b89c0edaf9b4134a89a562e8af7f24d
Faisol <faisol.wr@gmail.com>
2024-07-04 22:20:28 +0700

    [Update] login form

--- client/src/app/login/login.component.ts
+++ client/src/app/login/login.component.ts
@@ -1,12 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, FormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent {
+  // FormGroup
+  loginForm!: FormGroup;
+  // สร้างตัวแปรไว้เช็คว่า submit form หรือยัง
+  submitted = false;
 
+  // ตัวแปรสำหรับผูกกับฟอร์ม
+  userLogin = {
+    email: '',
+    password: '',
+  };
+
+  private fb = inject(FormBuilder);
+
+  ngOnInit() {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+  submitLogin() {
+    this.submitted = true;
+    if (this.loginForm.invalid) {
+      return;
+    } else {
+      alert('Login success')
+      this.userLogin.email = this.loginForm.value.email;
+      this.userLogin.password = this.loginForm.value.password;
+    }
+  }
+
+  resetForm() {
+    this.submitted = false;
+    this.loginForm.reset();
+  }
 }
