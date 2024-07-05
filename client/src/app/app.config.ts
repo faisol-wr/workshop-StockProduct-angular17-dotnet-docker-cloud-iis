6eb2726354c997fd7f61d36a2f9ad2de72d646ed
Faisol <faisol.wr@gmail.com>
2024-07-05 21:34:49 +0700

    [Update] config change route to top page and view transition

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,8 +1,24 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {
+  InMemoryScrollingOptions,
+  provideRouter,
+  withInMemoryScrolling,
+  withViewTransitions,
+} from '@angular/router';
 
 import { routes } from './app.routes';
 
+const scrollConfig: InMemoryScrollingOptions = {
+  scrollPositionRestoration: 'top',
+  anchorScrolling: 'enabled',
+};
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(
+      routes,
+      withInMemoryScrolling(scrollConfig),
+      withViewTransitions()
+    ),
+  ],
 };
