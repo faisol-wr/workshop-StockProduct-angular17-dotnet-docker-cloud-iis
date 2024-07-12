80a3dc956de1ecf8fb5548def8b9ca64c7a1f6ef
Faisol <faisol.wr@gmail.com>
2024-07-12 22:19:14 +0700

    [Create] server - model TbBook

---
+++ server/Models/TbBook.cs
@@ -0,0 +1,9 @@
+namespace ControllerServer
+{
+    public class TbBookModel
+    {
+        public int id { get; set; }
+        public string? name { get; set; }
+        public int price { get; set; }
+    }
+}
