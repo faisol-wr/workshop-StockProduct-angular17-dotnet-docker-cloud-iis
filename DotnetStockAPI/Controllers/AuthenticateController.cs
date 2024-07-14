04cf0af8bb52edb99b9b109bc878dc0b944dacb7
Faisol <faisol.wr@gmail.com>
2024-07-14 09:41:52 +0700

    [Update] DotnetStock - api register

--- DotnetStockAPI/Controllers/AuthenticateController.cs
+++ DotnetStockAPI/Controllers/AuthenticateController.cs
@@ -1,5 +1,10 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using DotnetStockAPI.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 
 namespace DotnetStockAPI.Controllers;
 
@@ -10,10 +15,23 @@ public class AuthenticateController : ControllerBase
     // สร้าง Object ของ ApplicationDbContext
     private readonly ApplicationDbContext _context;
 
+    // สร้าง Object จัดการ Users
+    private readonly UserManager<IdentityUser> _userManager;
+    private readonly RoleManager<IdentityRole> _roleManager;
+    private readonly IConfiguration _configuration;
+
     // ฟังก์ชันสร้าง Constructor สำหรับ initial ค่าของ ApplicationDbContext
-    public AuthenticateController(ApplicationDbContext context)
+    public AuthenticateController(
+        ApplicationDbContext context,
+        UserManager<IdentityUser> userManager,
+        RoleManager<IdentityRole> roleManager,
+        IConfiguration configuration
+    )
     {
         _context = context;
+        _userManager = userManager;
+        _roleManager = roleManager;
+        _configuration = configuration;
     }
 
     [HttpGet("testconnectdb")]
@@ -28,4 +46,87 @@ public class AuthenticateController : ControllerBase
             Response.WriteAsync("Not Connected");
         }
     }
+
+    // Register for User
+    // Post api/authenticate/register-user
+    [HttpPost]
+    [Route("register-user")]
+    public async Task<ActionResult> RegisterUser([FromBody] RegisterModel model)
+    {
+        // เช็คว่า username ซ้ำหรือไม่
+        var userExists = await _userManager.FindByNameAsync(model.Username);
+        if (userExists != null)
+        {
+            return StatusCode(
+                StatusCodes.Status500InternalServerError,
+                new ResponseModel
+                {
+                    Status = "Error",
+                    Message = "User already exists!"
+                }
+            );
+        }
+
+        // เช็คว่า email ซ้ำหรือไม่
+        userExists = await _userManager.FindByEmailAsync(model.Email);
+        if (userExists != null)
+        {
+            return StatusCode(
+                StatusCodes.Status500InternalServerError,
+                new ResponseModel
+                {
+                    Status = "Error",
+                    Message = "Email already exists!"
+                }
+            );
+        }
+
+        // สร้าง User
+        IdentityUser user = new()
+        {
+            Email = model.Email,
+            SecurityStamp = Guid.NewGuid().ToString(),
+            UserName = model.Username
+        };
+
+        // สร้าง User ในระบบ
+        var result = await _userManager.CreateAsync(user, model.Password);
+
+        // ถ้าสร้างไม่สำเร็จ
+        if (!result.Succeeded)
+        {
+            return StatusCode(
+                StatusCodes.Status500InternalServerError,
+                new ResponseModel
+                {
+                    Status = "Error",
+                    Message = "User creation failed! Please check user details and try again."
+                }
+            );
+        }
+
+        // กำหนด Roles Admin, Manager, User
+        if (!await _roleManager.RoleExistsAsync(UserRolesModel.Admin))
+        {
+            await _roleManager.CreateAsync(new IdentityRole(UserRolesModel.Admin));
+        }
+
+        if (!await _roleManager.RoleExistsAsync(UserRolesModel.Manager))
+        {
+            await _roleManager.CreateAsync(new IdentityRole(UserRolesModel.Manager));
+        }
+
+        if (await _roleManager.RoleExistsAsync(UserRolesModel.User))
+        {
+            await _roleManager.CreateAsync(new IdentityRole(UserRolesModel.User));
+            await _userManager.AddToRoleAsync(user, UserRolesModel.User);
+        }
+
+        return Ok(new ResponseModel
+        {
+            Status = "Success",
+            Message = "User registered successfully"
+        });
+    }
+
 }
