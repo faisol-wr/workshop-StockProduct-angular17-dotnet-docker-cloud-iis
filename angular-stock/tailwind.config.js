ecd7a2558b08455a8de5d5325f4aba4a9957337d
Faisol <faisol.wr@gmail.com>
2024-07-16 21:19:52 +0700

    [Update] AngularStock - tailwind setup font

--- angular-stock/tailwind.config.js
+++ angular-stock/tailwind.config.js
@@ -6,6 +6,9 @@ module.exports = {
       gridTemplateColumns: {
         fluid: "repeat(auto-fit, minmax(11rem, 1fr))",
       },
+      fontFamily: {
+        sans: ["Poppins", "Noto Sans Thai", ...defaultTheme.fontFamily.sans],
+      },
     },
   },
   plugins: [],
