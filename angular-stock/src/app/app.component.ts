205b4472a3afa107676fbc5579e6c302fe690f86
Faisol <faisol.wr@gmail.com>
2024-07-16 22:06:02 +0700

    [Update] AngularStock - route add path login

--- angular-stock/src/app/app.component.ts
+++ angular-stock/src/app/app.component.ts
@@ -27,7 +27,7 @@ export class AppComponent {
   title = 'angular-stock'
 
   isExpanded = true
-  isLoggedIn = true
+  isLoggedIn = false
 
   @ViewChild('sidenav', { static: true }) sidenav: any
 
--- angular-stock/src/app/app.routes.ts
+++ angular-stock/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'login' },
+  {
+    path: 'login',
+    component: LoginComponent,
+    data: { title: 'Login' },
+  },
+];
