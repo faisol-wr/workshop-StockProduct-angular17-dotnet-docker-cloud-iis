0db2c9f31c863a32dc5a369bbb29952942b72b27
Faisol <faisol.wr@gmail.com>
2024-07-21 20:55:54 +0700

    [Update] AngularStock - environment api url

--- angular-stock/src/environments/environment.prod.ts
+++ angular-stock/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
-    dotnet_api_url: 'http://localhost:8888/api/',
-    dotnet_api_url_image: 'http://localhost:8888/wwwroot/uploads/',
+    dotnet_api_url: 'http://localhost:5070/api/',
+    dotnet_api_url_image: 'http://localhost:5070/wwwroot/uploads/',
     base_url: '',
     token: '',
     version: '1.0',
