44af08774a8e10bef2d4e280545013183e834f1b
Faisol <faisol.wr@gmail.com>
2024-08-03 22:16:22 +0700

    [Update] netify setup

--- Docker/client/src/environments/environment.prod.ts
+++ Docker/client/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  dotnet_api_url: 'http://localhost:8081/api/',
-  dotnet_api_url_image: 'http://localhost:8081/uploads/',
+  dotnet_api_url: 'https://workshop-angular17-dotnet-1.onrender.com/api/',
+  dotnet_api_url_image: 'https://workshop-angular17-dotnet-1.onrender.com/uploads/',
   base_url: '',
   token: '',
   version: '1.0',
