79d94c3fa9a18c38888d09a80cb3d310a3d63a6f
Faisol <faisol.wr@gmail.com>
2024-08-03 21:31:33 +0700

    [Update] setup to render

--- Docker/server/Program.cs
+++ Docker/server/Program.cs
@@ -11,7 +11,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnectionRender"));
+    // options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
 // Adding Identity
